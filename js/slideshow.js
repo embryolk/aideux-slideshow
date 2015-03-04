b31fc7eaf3dfed752cdf95d1dffe7216937d7b1c
Jesse Lee <mienaikoe@gmail.com>
2015-03-03 23:55:28 -0500

    slideshow fixes

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,40 +3,52 @@ var Slideshow = function(outfits, container){
 	this.container.css("width", (outfits.length * 100) + "%");
 	
 	this.images = [];
+	var self = this;
 	for( var i in outfits ){
 		//var div = $("<div/>",{"class": 'slide'}).appendTo(container);
 		var div = $("<img/>",{"class": 'slide',"src": outfits[i].large}).appendTo(container);
 		//$("<img/>",{"src": outfits[i].large}).appendTo(div);
+				
+		//ornaments
+		var ornaments = $("<div/>",{"class":"ornaments"}).appendTo(container);
+		$("<a/>",{"class": 'fa fa-facebook','href': outfits[i].top}).appendTo(ornaments);
+		$("<a/>",{"class": 'fa fa-pinterest-p', 'href': outfits[i].bottom}).appendTo(ornaments);
+		var ribbonTop = $("<div/>",{"class": 'ribbon top'}).appendTo(ornaments);
+		$("<div/>",{class:"phrase type",text:"The Top"}).appendTo(ribbonTop);
+		$("<div/>",{class:"phrase title",text:outfits[i].top}).appendTo(ribbonTop);
+		var ribbonBottom = $("<div/>",{"class": 'ribbon bottom'}).appendTo(ornaments);
+		$("<div/>",{class:"phrase type",text:"The Bottom"}).appendTo(ribbonBottom);
+		$("<div/>",{class:"phrase title",text:outfits[i].bottom}).appendTo(ribbonBottom);
 		
-		// ribbons
-		var ribbons = $("<div/>",{"class":"ribbons"}).appendTo(container);
-		$("<div/>",{"class": 'ribbon top','text': outfits[i].top}).appendTo(ribbons);
-		$("<div/>",{"class": 'ribbon bottom', 'text': outfits[i].bottom}).appendTo(ribbons);
+		this.images.push({ src: outfits[i], div: div });
 		
-		this.images.push({
-			src: outfits[i],
-			div: div
-		});
+		div.click(function(idx){ return function(ev){ self.goto(idx); }; }(i) );
 	}
 	
 	this.currentLocation = 0;
+	$(window).load(function(ev){
+		self.constrainLeft();
+	});
 	
-	var self = this;
 	$(container).keydown(function(ev){
-		var move = false;
 		if( ev.which === 37 ) { // left
 			self.retreat();
 		} else if (ev.which === 39) { // right
 			self.advance();
 		}
 	}).click(function(ev){
+		/*
 		if( ev.pageX < ($(window).width()/2) ){
 			self.retreat();
 		} else {
 			self.advance();
 		}
+		*/
 	});
 	
+	$( window ).resize(function() {
+		self.center(self.currentLocation);
+	});
 };
 
 Slideshow.prototype = {
@@ -46,18 +58,37 @@ Slideshow.prototype = {
 		} else {
 			this.currentLocation = 0;
 		}
-		this.goto(this.images[this.currentLocation]);
+		this.goto(this.currentLocation);
 	},
 	retreat: function(){
 		if( this.currentLocation > 0 ){
 			this.currentLocation--;
-			this.goto(this.images[this.currentLocation]);
+			this.goto(this.currentLocation);
 		}
 	},
-	goto: function(image){
+	leftiness: function(location){
+		var image = this.images[location];
+		var leftiness = -image.div.position().left;
+		leftiness += $(window).width()/2;
+		leftiness -= image.div.width()/2;
+		return leftiness;
+	},
+	goto: function(location){
+		this.currentLocation = location;
 		this.container.animate({
-			"left": -image.div.position().left
+			"left": this.leftiness(location)
 		}, 500);
+	},
+	center: function(location){
+		this.currentLocation = location;
+		this.container.css({
+			"left": this.leftiness(location)
+		});
+	},
+	constrainLeft: function(){
+		this.container.css({
+			"left": 0
+		});
 	}
 };
 
